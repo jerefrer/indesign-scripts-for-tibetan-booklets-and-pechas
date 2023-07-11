1c636717fdeb4503b569466b065c7fb4c1a883bd
Jérémy FRERE <frere.jeremy@gmail.com>
2023-07-11 13:24:37 +0200

    Fixes addRinchenSpungShad to preserve anchoredObjects

--- addRinchenSpungShad.jsx
+++ addRinchenSpungShad.jsx
@@ -1,3 +1,7 @@
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+
 app.findGrepPreferences=app.changeGrepPreferences=null;
 app.findGrepPreferences.findWhat="༑";
 app.changeGrepPreferences.changeTo="།";
@@ -7,8 +11,14 @@ app.findGrepPreferences.findWhat="ང་";
 app.changeGrepPreferences.changeTo="ང༌";
 app.activeDocument.changeGrep();
 
-var lines = app.selection[0].paragraphs[0].lines.everyItem().getElements();
-while (lines.length) {
-    var line = lines.shift();
-    line.contents = line.contents.replace(/^([^་༌།༔  ]+[་༌]?)།([^།])/, "$1༑$2");
+var paragraphs = app.selection[0].paragraphs.everyItem().getElements();
+while (paragraphs.length) {
+    var paragraph = paragraphs.shift();
+    var lines = paragraph.lines.everyItem().getElements();
+    while (lines.length) {
+        var line = lines.shift();
+        var match = line.contents.match(/^([^་༌།༔  ]+[་༌]?)།([^།])/);
+        if (match)
+          line.contents = line.contents.replaceAt(match.index + match[0].length-2, '༑');
+    }
 }
