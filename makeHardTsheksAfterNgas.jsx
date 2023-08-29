e47441164c315d339a928b6acabad60869575d87
Jérémy FRERE <frere.jeremy@gmail.com>
2023-08-29 09:39:42 +0200

    Adds makeHardTsheksAfterNgas.jsx

---
+++ makeHardTsheksAfterNgas.jsx
@@ -0,0 +1,9 @@
+app.findGrepPreferences = app.changeGrepPreferences = null;
+
+app.findGrepPreferences.findWhat="ང་";
+app.changeGrepPreferences.changeTo="ང༌";
+app.activeDocument.changeGrep();
+
+app.findGrepPreferences.findWhat="་{2,}";
+app.changeGrepPreferences.changeTo="་";
+app.activeDocument.changeGrep();
