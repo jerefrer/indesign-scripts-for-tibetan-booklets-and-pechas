d93e4e0b0855d9594b694af7d34e07b753ec3b76
Jérémy FRERE <frere.jeremy@gmail.com>
2024-04-23 12:59:39 +0100

    Now uses the real tibskrit-transliterator-app repo and calls it with node, so it can be kept up to date more easily

--- InsertPhonetics/lib/insert-mantra.js
+++ InsertPhonetics/lib/insert-mantra.js
@@ -1,6 +1,7 @@
 #include "./json2.js"
 #include "./shared.js"
-#include "./tibkrit-transliterator/tibkrit-transliterator.js"
+
+var scriptDirectory = File($.fileName).parent.fsName;
 
 var document = app.activeDocument;
 var selection = app.selection[0];
@@ -25,8 +26,7 @@ function insertMantra(isPhonetics) {
     var followingParagraph = paragraph.insertionPoints.item(-1).paragraphs[0];
     // if (paragraph.appliedParagraphStyle === tibetanStyle && (!followingParagraph.isValid || followingParagraph.appliedParagraphStyle !== mantraStyle)) {
     if (paragraph.appliedParagraphStyle === tibetanStyle) {
-      var tibkrit = new TibkritTransliterator(paragraph.contents);
-      var transliteration = tibkrit.transliterate({ capitalize: true, phonetics: isPhonetics }).replace(/[\r\n]/g, '');
+      var transliteration = generateIASTFor(paragraph.contents, isPhonetics).replace(/[\r\n]/g, '');
       if (transliteration.replace(/\s/g, '').length === 0) {
         return;
       }
@@ -39,4 +39,22 @@ function insertMantra(isPhonetics) {
       transliterationParagraph.appliedParagraphStyle = mantraStyle;
     }
   }
+}
+
+function generateIASTFor(tibetan, isPhonetics) {  
+  var inputFilePath = "/tmp/mantras_input.txt";
+  var inputFile = new File(inputFilePath);
+
+  inputFile.open("w");
+  inputFile.encoding = "utf-8";
+  inputFile.write(tibetan);
+  inputFile.close();
+
+  var appleScript =
+  'set nodePath to "/Users/jeremyfrere/.nvm/versions/node/v21.6.1/bin/node"\r' +
+  'set command to nodePath & " ' + scriptDirectory + '/tibskrit-transliterator-cli.js /tmp/mantras_input.txt ' + (isPhonetics ? 'true' : 'false') + '"\r' +
+  'set output to do shell script command\r' +
+  'return output\r';
+
+  return app.doScript(appleScript, ScriptLanguage.APPLESCRIPT_LANGUAGE);
 }
