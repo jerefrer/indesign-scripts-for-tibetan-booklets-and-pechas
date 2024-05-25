b406be7c92f8987abec7e9fdaefcb29a9ffae00d
Jérémy FRERE <frere.jeremy@gmail.com>
2024-05-25 20:54:04 +0100

    Fixes RinchenSpungShad function and adds two modes: whole syllable or single column.

--- Pechas/Add Rinchen Spung Shad.jsx
+++ Pechas/Add Rinchen Spung Shad.jsx
@@ -7,29 +7,34 @@ app.findGrepPreferences.findWhat = "༑";
 app.changeGrepPreferences.changeTo = "།";
 app.activeDocument.changeGrep();
 
-var paragraphs = app.selection[0].paragraphs.everyItem().getElements();
+var paragraphs = app.selection[0].parentStory.paragraphs.everyItem().getElements();
 var allLines = [];
-while (paragraphs.length) {
-    var paragraph = paragraphs.shift();
+for (var j = 0; j < paragraphs.length; j++) {
+    var paragraph = paragraphs[j];
     var lines = paragraph.lines.everyItem().getElements();
-    allLines.concat(lines);
+    allLines = allLines.concat(lines);
 }
 
 var i = 0;
 var progressBar = new $.ProgressBar("Rinchen Spung Shad", 350, 100);
-progressBar.show("Processing line %1 / " + lines.length, lines.length, i);
+progressBar.show("Processing line %1 / " + allLines.length, allLines.length, i);
 
-for(; i < lines.length; i++) {
-    var line = lines[i];
-    var match = line.contents.match(/^([^་༌།༔  ]+[་༌]?)།([^།])/);
-    if (match) {
-        app.findGrepPreferences = app.changeGrepPreferences = null;
-        app.findGrepPreferences.findWhat = "("+match[1]+")།([^།])(.*)";
-        app.changeGrepPreferences.changeTo = "$1༑$2$3";
-        line.changeGrep();
-        progressBar.hitValue(i);
-        progressBar.hit(i);
-        $.sleep(100);
+// var regexp = /^\uFEFF?([^་༌།༔  ]+[་༌]?)།([^།])/; // Any syllable followed by shed
+var regexp = /^\uFEFF?^([ཀཁགངཅཆཇཉཏཊཐཋདཌནཎཔཕབམཙཚཛཝཞསའཡརཤཥཟཧཨ][ཱ]?[ེིོུ]?[ཾྃྂ]?[་༌]?)།([^།])/; // Single "column" syllable followed by shed
+
+for(; i < allLines.length; i++) {
+    var line = allLines[i];
+    if (!line.contents.match(/^\uFEFF?༄༅/)) {
+        var match = line.contents.match(regexp);
+        if (match) {
+            app.findGrepPreferences = app.changeGrepPreferences = null;
+            app.findGrepPreferences.findWhat = "("+match[1]+")།([^།])(.*)";
+            app.changeGrepPreferences.changeTo = "$1༑$2$3";
+            line.changeGrep();
+            progressBar.hitValue(i);
+            progressBar.hit(i);
+            $.sleep(100);
+        }
     }
 }
 
