c30a97f62c1c0947341379f60286d0f55b58f979
Jérémy FRERE <frere.jeremy@gmail.com>
2024-05-25 20:43:52 +0100

    Fixes MakeHardTshegsAfterNgas to handle ter tshegs and accents/anusvaras

--- Make Hard Tshegs After Ngas.jsx
+++ Make Hard Tshegs After Ngas.jsx
@@ -1,9 +1,5 @@
 app.findGrepPreferences = app.changeGrepPreferences = null;
 
-app.findGrepPreferences.findWhat="ང་";
-app.changeGrepPreferences.changeTo="ང༌";
-app.activeDocument.changeGrep();
-
-app.findGrepPreferences.findWhat="་{2,}";
-app.changeGrepPreferences.changeTo="་";
+app.findGrepPreferences.findWhat="ང[ེིོུ]?[ཾྃྂ]?་([།༔])";
+app.changeGrepPreferences.changeTo="ང༌$1";
 app.activeDocument.changeGrep();
